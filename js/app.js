8b7139a3d2254883d4981c9742c0f34918a1e801
William Almnes <william.francis.daraman@vg.no>
2015-01-22 11:56:38 +0100

    Added support for multiple devices. Working for iPhone 6 and iPad.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,65 @@
 $(function () {
-    var $deviceContainer = $('#device-container'),
+    var currentDevice = 'iphone6',
+        rotated = false,
+        spec = [{
+            device: 'iphone6',
+            css: {
+                normal: '160px',
+                rotate: '292px',
+                width: '375px',
+                height: '667px'
+            },
+        },
+        {
+            device: 'ipad',
+            css: {
+                normal: '160px',
+                rotate: '0',
+                width: '1024px',
+                height: '768px'
+            }
+        }
+        ],
+        $deviceContainer = $('#device-container'),
         $iframeEl = $deviceContainer.find('iframe');
 
     var changeIframeSrc = function () {
         // TODO: Check if valid URL.
         $iframeEl.attr('src', $('#iframeURLInput').val());
     };
+
+    var getDeviceStyle = function () {
+        var deviceSpec;
+        spec.forEach(function (s) {
+            console.log('comparing ' + s.device + ' with ' + currentDevice);
+            if (s.device === currentDevice) {
+                deviceSpec = s;
+            }
+        });
+        console.log(deviceSpec);
+        return deviceSpec;
+     };
+
+    var setStyleAccordingToDevice = function () {
+        var deviceStyle = getDeviceStyle();
+            marginTop = deviceStyle.css.normal,
+            height = deviceStyle.css.height,
+            width = deviceStyle.css.width;
+
+        if (rotated) {
+            marginTop = deviceStyle.css.rotate;
+            height = deviceStyle.css.width;
+            width = deviceStyle.css.height;
+        }
+
+        $deviceContainer.css({
+            'margin-top': marginTop,
+            'width': width,
+            'height': height
+        });
+
+    };
+
 
 
     $('#deviceBtn-iphone6').on('click', function(e) {
@@ -15,6 +69,8 @@ $(function () {
         });
 
         $deviceContainer.addClass('device-iphone6');
+        currentDevice = 'iphone6';
+        setStyleAccordingToDevice();
     });
 
     $('#deviceBtn-ipad').on('click', function(e) {
@@ -23,37 +79,27 @@ $(function () {
             return (css.match (/(^|\s)device-\S+/g) || []).join(' ');
         });
         $deviceContainer.addClass('device-ipad');
+        currentDevice = 'ipad';
+        setStyleAccordingToDevice();
     });
 
     $('#deviceBtn-rotate').on('click', function(e) {
         e.preventDefault();
         $deviceContainer.toggleClass('rotateCW');
+        rotated = !rotated;
 
         window.setTimeout(function() {
-            var tmpWidth = $deviceContainer.width();
-            var tmpHeight = $deviceContainer.height();
-            $deviceContainer.height(tmpWidth);
-            $deviceContainer.width(tmpHeight);
-            if ($deviceContainer.hasClass('rotateCW')) {
-                $deviceContainer.css({'margin-top': '292px'});
-            }
-            else {
-                $deviceContainer.css({'margin-top': '160px'});
-            }
+            setStyleAccordingToDevice();
 
             $deviceContainer.find('iframe').toggleClass('rotateCCW');
             $deviceContainer.toggleClass('rotate');
         }, 805);
 
-        // $deviceContainer.css({
-        //     '-webkit-transform': 'rotate(90deg)',
-        //    'transform': 'rotate(90deg)'});
     });
 
     $('#iframeURLInput').on('keydown', function (e) {
-        // If the user presses enter
+        // If the user presses enter.
         if (e.which === 13) {
-            console.log('Changing iframe src');
             changeIframeSrc();
         }
     });
